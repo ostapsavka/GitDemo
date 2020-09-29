4d8b07289a9b8e3e2c7d94dc7ffb06199217163a
Ostap Savka <ostapsawka@gmail.com>
2020-09-29 15:43:03 +0300

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by Sawka Ostap on 29.09.2020.
+//  Copyright © 2020 SavkaOstap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
